b8c0169d049f260db4564479ba9164216cd819b4
frzhilman27 <frzhilman27@gmail.com>
2026-02-26 00:29:18 +0700

    frontend ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import AppRouter from "./router/AppRouter";
+import { useEffect, useState } from "react";
+import API_URL from "./config";
 
 function App() {
-  return <AppRouter />;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend Connected</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const API_URL = "https://sipentar-backend-production-4843.up.railway.app";
+
+export default API_URL;
