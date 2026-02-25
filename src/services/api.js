95b4f2fc9a388a07fe2dfcbc07d622acf1c7b6eb
frzhilman27 <frzhilman27@gmail.com>
2026-02-25 20:33:46 +0700

    Fix production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://sipentar-backend-production.up.railway.app"
 });
 
 export default API;
