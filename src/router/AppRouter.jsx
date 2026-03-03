4794d3b3ff02950190258e079730a9611252c6de
Faris Hilman Ramadhan <frzhi@example.com>
2026-03-03 14:39:45 +0700

    fix

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -8,16 +8,15 @@ import Laporan from "../pages/Laporan";
 
 function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-         <Route path="/dashboard" element={<ProtectedRoute roleRequired="admin"><Dashboard /></ProtectedRoute>}/>
-         <Route path="/laporan" element={<ProtectedRoute roleRequired="user"><Laporan /></ProtectedRoute>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<ProtectedRoute roleRequired="admin"><Dashboard /></ProtectedRoute>} />
+        <Route path="/laporan" element={<ProtectedRoute roleRequired="user"><Laporan /></ProtectedRoute>} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
